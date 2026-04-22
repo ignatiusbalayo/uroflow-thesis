99e817ecef2453ed3582c1061d4ec2f6f74b5f6c
wilfredKisitu <wktdcourses@gmail.com>
2026-04-22 11:25:36 +0300

    updated the parameter computation pipeline to precompute incase so values miss

--- utils/read_normalization_params.py
+++ utils/read_normalization_params.py
@@ -2,6 +2,7 @@ import joblib
 from configs.path_configs import NORMALIZATION_PARAMS_PATH
 from Dataset.devices import Device
 from enum import Enum
+from utils.compute_rescale_params import compute_rescale_configs_params
 
 class Transform_keys(Enum):
     LFT = 'lft'
@@ -13,18 +14,26 @@ class Transform_keys(Enum):
         return list(cls)
 
 
-def  read_data_one(device: Device, transform_key: Transform_keys):
+def  read_norm_data(device: Device, transform_key: Transform_keys):
     """Reads the data for a given transform """
-    state_obj = joblib.load(NORMALIZATION_PARAMS_PATH)
+    try:
+        state_obj = joblib.load(NORMALIZATION_PARAMS_PATH)
+    except:
+        state_obj = compute_rescale_configs_params()
     temp = state_obj[device.value][transform_key.value]
     return temp
 
 
-
 if __name__ == '__main__':
     import os
+    import joblib
+
     os.system('clear')
 
-    state_obj = read_data_one(Device.UM, Transform_keys.Mel_2d)
-    print(state_obj)
+    temp  = read_norm_data(Device.PHONE, Transform_keys.Mel_2d)
+    print(f'Normalization data for device {Device.UM.value}', temp, sep='\n')
+
+
+
+
 
